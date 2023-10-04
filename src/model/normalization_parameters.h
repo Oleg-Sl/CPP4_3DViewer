02b36861aa2afb2ddeead020713a91cbfcaf2eaa
Oleg <slepcov_oleg@mail.ru>
2023-10-04 19:47:03 +0700

    Added header guards

---
+++ src/model/normalization_parameters.h
@@ -0,0 +1,11 @@
+#ifndef _3DVIEWER_MODEL_NORMALIZATION_PARAMETERS_H_
+#define _3DVIEWER_MODEL_NORMALIZATION_PARAMETERS_H_
+
+struct NormalizationParameters {
+    float min;
+    float max;
+    float dx_step;
+    float dy_step;
+};
+
+#endif  // _3DVIEWER_MODEL_NORMALIZATION_PARAMETERS_H_
