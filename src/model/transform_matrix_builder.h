eb32fb30baf1b2158a8642d9acf069f73ebb6f11
Oleg <slepcov_oleg@mail.ru>
2023-10-04 20:15:09 +0700

    Added header guards

--- src/model/transform_matrix_builder.h
+++ src/model/transform_matrix_builder.h
@@ -1,3 +1,5 @@
+#ifndef _3DVIEWER_MODEL_TRANSFORM_MATRIX_BUILDER_H_
+#define _3DVIEWER_MODEL_TRANSFORM_MATRIX_BUILDER_H_
 
 #include "point_3d.h"
 
@@ -8,3 +10,5 @@ class TransformMatrixBuilder {
     static TransformMatrix CreateMoveMatrix(float x, float y, float z);
     static TransformMatrix CreateScaleMatrix(float x, float y, float z);
 };
+
+#endif  // _3DVIEWER_MODEL_TRANSFORM_MATRIX_BUILDER_H_
