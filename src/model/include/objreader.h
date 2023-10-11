340646faf9812b4c0d0909486f46b0861bb8a235
Alexander <varlybot@student.21-school.ru>
2023-10-11 14:45:34 +0700

    rename filereader -> ojbreader

---
+++ src/model/include/objreader.h
@@ -0,0 +1,34 @@
+#ifndef _3DVIEWER_MODEL_FILEREADER_H_
+#define _3DVIEWER_MODEL_FILEREADER_H_
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "basefilereader.h"
+#include "normalization_parameters.h"
+#include "scene.h"
+
+namespace s21 {
+
+class OBJReader : BaseFileReader {
+ public:
+  const std::string kVertexToken = "v";
+  const std::string kFaceToken = "f";
+
+ public:
+  Scene ReadScene(
+      const std::string &path,
+      const NormalizationParameters &normalization_parameters) override;
+
+ private:
+  Vertex ReadVertex(std::stringstream &tokens);
+  Figure ReadFace(std::stringstream &tokens,
+                  const std::vector<Vertex> &vertices);
+  void NormalizationScene(Scene &scene, const NormalizationParameters &params);
+  void CalculateSceneBounds(Scene &scene, const Vertex &vertex);
+};
+
+}  // namespace s21
+
+#endif  // _3DVIEWER_MODEL_FILEREADER_H_
---
+++ src/model/objreader.cc
@@ -0,0 +1,148 @@
+#include "include/objreader.h"
+
+#include <fstream>
+#include <iostream>
+
+namespace s21 {
+
+Vertex OBJReader::ReadVertex(std::stringstream& tokens) {
+  float x, y, z;
+  tokens >> x >> y >> z;
+  return Vertex({x, y, z});
+}
+
+Figure OBJReader::ReadFace(std::stringstream& tokens,
+                           const std::vector<Vertex>& vertices) {
+  Figure figure;
+  int vertex_index;
+  std::string line;
+
+  while (tokens >> line) {
+    std::string token;
+
+    size_t slash_pos = line.find("//");
+
+    if (slash_pos != std::string::npos) {
+      token = line.substr(0, slash_pos);
+    } else {
+      token = line;
+    }
+
+    vertex_index = std::stoi(token);
+
+    if (vertex_index < 0) {
+      vertex_index = vertices.size() + vertex_index;
+    } else {
+      vertex_index -= 1;
+    }
+    if (vertex_index < 0 || vertex_index >= static_cast<int>(vertices.size())) {
+      throw std::invalid_argument("Invalid face index: " +
+                                  std::to_string(vertex_index));
+    }
+
+    figure.AddVertex(vertices[vertex_index]);
+  }
+
+  for (size_t i = 0; i < figure.GetVertices().size(); ++i) {
+    size_t end = i + 1 == figure.GetVertices().size() ? 0 : i + 1;
+    figure.AddEdge(i, end);
+  }
+
+  return figure;
+}
+
+void OBJReader::NormalizationScene(Scene& scene,
+                                   const NormalizationParameters& params) {
+  Bounds scene_bounds = scene.GetBounds();
+
+  if (scene_bounds.max_vertex_x_value - scene_bounds.min_vertex_x_value != 0 &&
+      scene_bounds.max_vertex_y_value - scene_bounds.min_vertex_y_value != 0 &&
+      scene_bounds.max_vertex_z_value - scene_bounds.min_vertex_z_value != 0) {
+    float scale_x =
+        (params.max - params.min) /
+        (scene_bounds.max_vertex_x_value - scene_bounds.min_vertex_x_value);
+    float scale_y =
+        (params.max - params.min) /
+        (scene_bounds.max_vertex_y_value - scene_bounds.min_vertex_y_value);
+    float scale_z =
+        (params.max - params.min) /
+        (scene_bounds.max_vertex_z_value - scene_bounds.min_vertex_z_value);
+
+    for (Figure& figure : scene.GetFigures()) {
+      for (Vertex& vertex : figure.GetVertices()) {
+        float new_x_position =
+            (vertex.GetPosition().x - scene_bounds.min_vertex_x_value) *
+                scale_x +
+            params.min;
+
+        float new_y_position =
+            (vertex.GetPosition().y - scene_bounds.min_vertex_y_value) *
+                scale_y +
+            params.min;
+
+        float new_z_position =
+            (vertex.GetPosition().z - scene_bounds.min_vertex_z_value) *
+                scale_z +
+            params.min;
+
+        vertex.SetPosition({new_x_position, new_y_position, new_z_position});
+      }
+    }
+  }
+}
+
+void OBJReader::CalculateSceneBounds(Scene& scene, const Vertex& vertex) {
+  Bounds scene_bounds = scene.GetBounds();
+
+  scene_bounds.min_vertex_x_value =
+      std::min(vertex.GetPosition().x, scene_bounds.min_vertex_x_value);
+  scene_bounds.min_vertex_y_value =
+      std::min(vertex.GetPosition().y, scene_bounds.min_vertex_y_value);
+  scene_bounds.min_vertex_z_value =
+      std::min(vertex.GetPosition().z, scene_bounds.min_vertex_z_value);
+
+  scene_bounds.max_vertex_x_value =
+      std::max(vertex.GetPosition().x, scene_bounds.max_vertex_x_value);
+  scene_bounds.max_vertex_y_value =
+      std::max(vertex.GetPosition().y, scene_bounds.max_vertex_y_value);
+  scene_bounds.max_vertex_z_value =
+      std::max(vertex.GetPosition().z, scene_bounds.max_vertex_z_value);
+
+  scene.SetBounds(scene_bounds);
+}
+
+Scene OBJReader::ReadScene(const std::string& path,
+                           const NormalizationParameters& params) {
+  Scene scene;
+  std::vector<Vertex> vertices;
+  std::ifstream obj_file(path);
+
+  if (obj_file.is_open()) {
+    std::string curr_line;
+
+    while (std::getline(obj_file, curr_line)) {
+      std::stringstream tokens(curr_line);
+      std::string curr_token;
+      tokens >> curr_token;
+
+      if (curr_token == kVertexToken) {
+        Vertex vertex = ReadVertex(tokens);
+        vertices.push_back(vertex);
+        CalculateSceneBounds(scene, vertex);
+      } else if (curr_token == kFaceToken) {
+        Figure figure = ReadFace(tokens, vertices);
+        scene.AddFigure(std::move(figure));
+      }
+    }
+
+    obj_file.close();
+  } else {
+    throw std::runtime_error("Failed to open file: " + path);
+  }
+
+  NormalizationScene(scene, params);
+
+  return scene;
+}
+
+}  // namespace s21
